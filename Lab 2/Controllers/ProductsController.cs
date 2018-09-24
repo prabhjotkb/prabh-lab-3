fe339c5c7bcdbf8130b9ad18ec5764e2e63c5a7c
Prabhjot Kaur Bhathal <kaurprabhjot1603@gmail.com>
2018-09-24 17:46:52 -0400

    descriptive message here

---
+++ Lab 2/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET : Products/World
+        public ActionResult World(string WorldName)
+        {
+            ViewBag.WorldName = WorldName;
+            return View();
+        }
+    }
+}
